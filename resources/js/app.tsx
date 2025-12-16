a24b329ddc5113a58e7038ce1201148bc51a6176
mainaerick <niteric@gmail.com>
2025-12-17 01:11:08 +0300

    fix: remove app name from page title

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -9,7 +9,7 @@ import {ConfigProvider} from "antd";
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.tsx`,
